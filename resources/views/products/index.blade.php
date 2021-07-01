74a17a7a52f348096c4da551c75a57164abc5a60
FachrulAndySetyawan <alungaregganteng@gmail.com>
2021-07-01 22:23:02 +0700

    PTW-0140-0146

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section ('content')
+
+<div class="container p-0">
+  <div class="row">
+
+    <div class="col-lg-3 col-md-3 col-sm-4 col-5 pl-4 filter">
+      <div class="fixedfilter">
+        <h3><i class="fa fa-filter"></i> Filter </h3>
+        <input class="mt-3" type="text" id="search" placeholder="Enter product name" style="width:100%;">
+        </div>
+    </div>
+    <div class="col-lg-9 col-md-9 col-sm-8 col-7 pr-4">
+      <h3>Product</h3>
+      
+      <div class="row d-flex justify-content-start" id="products">
+        
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+
+
+@endsection
